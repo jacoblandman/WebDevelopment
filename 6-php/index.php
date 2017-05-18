516d60158ee1048d481faa4f365b2d10674b0a3f
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-18 11:56:01 -0500

    Finished bootstrap section and starting php section

---
+++ 6-php/index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "Hello World\n";
+
+// variables
+$name = "Jacob";
+echo "My name is $name\n";
+
+$string1 = "This is the first part";
+$string2 = " of a sentence";
+
+echo "$string1$string2\n";
+
+$myNumber = 45;
+
+// don't have to be careful about the typ eof the variable
+$calculation = $myNumber * 31 / 97 + 4;
+
+echo "The caluclation is ".$calculation."\n";
+
+$myBool = true;
+
+echo "<p>This statment is true? ".$myBool."</p>\n";
+
+$variableName = "name";
+
+echo $$variableName."\n";
+
+
+// arrays
+
+?>
